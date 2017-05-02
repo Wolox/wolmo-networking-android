cabf355de78364b51d8b2d03768b6fb51dc23c9e
Juan Ignacio <juanignaciomln@gmail.com>
2017-05-02 17:42:12 -0300

    Added base classes to implement offline mechanisms

---
+++ networking/src/main/java/ar/com/wolox/wolmo/networking/BaseCallCollapser.java
@@ -0,0 +1,145 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2017 Wolox S.A
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package ar.com.wolox.wolmo.networking;
+
+import android.support.annotation.NonNull;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.concurrent.ConcurrentHashMap;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+/**
+ * Collapses GET requests into one by calling the first and reporting the result to every callback.
+ */
+public class BaseCallCollapser implements ICallCollapser {
+
+    private static final String HTTP_METHOD_GET = "GET";
+
+    private ConcurrentHashMap<String, Queue<Callback>> mGetCallbackQueues;
+
+    public BaseCallCollapser() {
+        mGetCallbackQueues = new ConcurrentHashMap<>();
+    }
+
+    /**
+     * Enqueues the call immediately if it's not a GET. This is done because only GET (read only)
+     * operations can be collapsed, writing operations should't be altered.
+     * <p>
+     * Collapsing the call means adding it to the queue of the same
+     * request, and executing it if it's the first to be added to said queue.
+     *
+     * @param call     to be enqueued
+     * @param callback to be called when executing it
+     */
+    public final <T> void enqueue(@NonNull Call<T> call, @NonNull Callback<T> callback) {
+        if (!isGetCall(call)) {
+            call.enqueue(callback);
+            return;
+        }
+
+        Queue<Callback> requestQueue = getQueueFromRequest(call);
+
+        requestQueue.add(callback);
+        if (requestQueue.size() > 1) return;
+        collapsingEnqueue(call);
+    }
+
+    private boolean isGetCall(Call call) {
+        return HTTP_METHOD_GET.equalsIgnoreCase(call.request().method());
+    }
+
+    /**
+     * Retrieves the request URL of the {@link Call} and returns a queue for it. If it doesn't exist
+     * yet, it creates it and adds it to {@link #mGetCallbackQueues}.
+     *
+     * @param call for the queue retrieval
+     * @return a {@link Queue} associated to the given {@link Call} url.
+     */
+    @NonNull
+    private Queue<Callback> getQueueFromRequest(Call call) {
+        String url = call.request().url().toString();
+
+        // Synchronization is needed here since it's safe per call but having multiple calls
+        // makes it vulnerable.
+        synchronized (mGetCallbackQueues) {
+            if (mGetCallbackQueues.containsKey(url)) return mGetCallbackQueues.get(url);
+        }
+
+        LinkedList<Callback> requestQueue = new LinkedList<>();
+        mGetCallbackQueues.put(url, requestQueue);
+        return requestQueue;
+    }
+
+    /**
+     * Calls {@link Call#enqueue(Callback)} with a {@link Callback<T>} that reports the result to
+     * the request queue.
+     *
+     * @param call to execute
+     */
+    private <T> void collapsingEnqueue(@NonNull Call<T> call) {
+        call.enqueue(new Callback<T>() {
+            @Override
+            public void onResponse(Call<T> call, Response<T> response) {
+                applySuccessToQueue(call, response);
+            }
+
+            @Override
+            public void onFailure(Call<T> call, Throwable t) {
+                applyFailureToQueue(call, t);
+            }
+        });
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> void applySuccessToQueue(@NonNull Call<T> call, @NonNull Response<T> response) {
+        Queue<Callback> requestQueue = getQueueFromRequest(call);
+
+        Callback currentCallback;
+        while ((currentCallback = requestQueue.poll()) != null) {
+            currentCallback.onResponse(call, response);
+        }
+
+        removeQueueFromRequest(call);
+    }
+
+    @SuppressWarnings("unchecked")
+    private <T> void applyFailureToQueue(@NonNull Call<T> call, @NonNull Throwable t) {
+        Queue<Callback> requestQueue = getQueueFromRequest(call);
+
+        Callback currentCallback;
+        while ((currentCallback = requestQueue.poll()) != null) {
+            currentCallback.onFailure(call, t);
+        }
+
+        removeQueueFromRequest(call);
+    }
+
+    private void removeQueueFromRequest(Call call) {
+        mGetCallbackQueues.remove(call.request().url().toString());
+    }
+
+}
---
+++ networking/src/main/java/ar/com/wolox/wolmo/networking/Repository.java
@@ -0,0 +1,241 @@
+/**
+ * MIT License
+ * <p>
+ * Copyright (c) 2017 Wolox S.A
+ * <p>
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ * and associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * <p>
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
+ * <p>
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+ * INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+ * TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+package ar.com.wolox.wolmo.networking;
+
+import android.support.annotation.IntDef;
+import android.support.annotation.IntRange;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import ar.com.wolox.wolmo.core.callback.WoloxCallback;
+import okhttp3.Cache;
+import okhttp3.ResponseBody;
+import retrofit2.Call;
+
+/**
+ * Handles the caching retrieval of a query and success/error notification regarding that operation.
+ * <p/>
+ * The main method is {@link #query} that, provided of a policy, the necessary information to
+ * retrieve data and notify the user, determines the action to take and notifies accordingly.
+ */
+public abstract class Repository {
+
+    public static final int INTERNAL_ERROR_CODE = 666;
+    public static final int CACHE_MISS_ERROR_CODE = 888;
+
+    /**
+     * Flags for cache interaction control.
+     */
+    @Retention(RetentionPolicy.SOURCE)
+    @IntDef({CACHE_NONE, CACHE_FIRST, CACHE_ONLY, TIME_RESOLVE})
+    protected @interface AccessPolicy {
+    }
+
+    /**
+     * Makes the queries never check cache.
+     */
+    protected static final int CACHE_NONE = 1;
+
+    /**
+     * Makes the queries check cache first and, if missed, make a request and update it.
+     */
+    protected static final int CACHE_FIRST = 2;
+
+    /**
+     * Makes the queries only check cache.
+     */
+    protected static final int CACHE_ONLY = 3;
+
+    /**
+     * Checks whether {@link #getRefreshDelta()} has passed. If it has, it refreshes cache by getting
+     * data from API. Else, it returns cache data.
+     */
+    protected static final int TIME_RESOLVE = 4;
+
+    private ICache mCache;
+    private long mLastRefreshMoment;
+    private ICallCollapser mCallCollapser;
+
+    /**
+     * Constructs a basic repository.
+     *
+     * @param cache to query for cached items
+     */
+    protected Repository(@NonNull ICache cache) {
+        mCache = cache;
+        mCallCollapser = new BaseCallCollapser();
+        mLastRefreshMoment = System.currentTimeMillis();
+    }
+
+    /**
+     * Constructs a basic repository with a custom {@link ICallCollapser}
+     *
+     * @param cache         to query for cached items
+     * @param callCollapser a custom {@link ICallCollapser} to handle http requests
+     */
+    protected Repository(@NonNull ICache cache, @NonNull ICallCollapser callCollapser) {
+        mCache = cache;
+        mCallCollapser = callCollapser;
+        mLastRefreshMoment = System.currentTimeMillis();
+    }
+
+    /**
+     * Queries the corresponding environment, with no update, in order to retrieve information and
+     * handle it to the user.
+     *
+     * @see #query(int, Class, Object, Call, IRepositoryCallback, IUpdate)
+     */
+    protected final <T> void query(@AccessPolicy int policy, @NonNull Class<T> clazz,
+                                   @NonNull Object key, @NonNull Call<T> call,
+                                   @NonNull IRepositoryCallback<T> callback) {
+        query(policy, clazz, key, call, callback, null);
+    }
+
+    /**
+     * Queries the corresponding environment in order to retrieve information and handle it to the
+     * user.
+     * <p/>
+     * Whether it checks cache, does the request and consequently notifies error is determined by
+     * {@link AccessPolicy}.
+     *
+     * @param policy   policy to use for the query
+     * @param clazz    {@link Class<T>} of the object to query
+     * @param key      to identify cache data
+     * @param call     request that retrieves asked information
+     * @param callback that notifies the result of the query
+     * @param update   optional update to apply
+     */
+    protected final <T> void query(@AccessPolicy int policy, @NonNull Class<T> clazz,
+                                   @NonNull Object key, @NonNull Call<T> call,
+                                   @NonNull IRepositoryCallback<T> callback,
+                                   @Nullable IUpdate<T> update) {
+        if (accessCache(policy)) {
+            T cachedData = update != null ? applyUpdate(clazz, key, update)
+                    : mCache.read(clazz, key);
+
+            if (cachedData != null) {
+                if (shouldInvalidateCache(policy)) {
+                    mCache.clear(clazz, key);
+                } else {
+                    callback.onSuccess(cachedData);
+                    return;
+                }
+            } else if (policy == CACHE_ONLY) {
+                callback.onError(CACHE_MISS_ERROR_CODE);
+                return;
+            }
+        }
+
+        fetchData(clazz, call, callback);
+    }
+
+    /**
+     * @param policy policy to check
+     * @return wether the policy indicates that the query should access the cache.
+     */
+    private boolean accessCache(@AccessPolicy int policy) {
+        return policy != CACHE_NONE;
+    }
+
+    /**
+     * Applies an update to the object in cache and updates the refresh timer.
+     *
+     * @param clazz  of the object
+     * @param key    to identify the element
+     * @param update to apply
+     * @return the updated element.
+     */
+    private <T> T applyUpdate(@NonNull Class<T> clazz, @NonNull Object key,
+                              @NonNull IUpdate<T> update) {
+        updateRefreshMoment();
+        return mCache.update(clazz, key, update);
+    }
+
+    /**
+     * Sets {@link #mLastRefreshMoment} to the current moment in time.
+     */
+    private void updateRefreshMoment() {
+        mLastRefreshMoment = System.currentTimeMillis();
+    }
+
+    /**
+     * @param policy policy taken
+     * @return whether cached data should be invalidated.
+     */
+    private boolean shouldInvalidateCache(@AccessPolicy int policy) {
+        return policy == TIME_RESOLVE &&
+                (System.currentTimeMillis() - mLastRefreshMoment) >= getRefreshDelta();
+    }
+
+    /**
+     * Makes a request and notifies accordingly. In case of success, it updates the {@link Cache}.
+     *
+     * @param clazz    {@link Class<T>} of the information to query
+     * @param call     request to be done
+     * @param callback that notifies the result of the query
+     * @throws IllegalStateException if the <code>call</code> is either executed or cancelled.
+     */
+    private <T> void fetchData(@NonNull final Class<T> clazz, @NonNull Call<T> call,
+                               @NonNull final IRepositoryCallback<T> callback) {
+        if (call.isExecuted() || call.isCanceled()) {
+            throw new IllegalStateException("Call should be ready to use");
+        }
+
+        mCallCollapser.enqueue(call, new WoloxCallback<T>() {
+            @Override
+            public void onResponseSuccessful(T data) {
+                mCache.save(clazz, data);
+                updateRefreshMoment();
+                callback.onSuccess(data);
+            }
+
+            @Override
+            public void onResponseFailed(ResponseBody responseBody, int code) {
+                callback.onError(code);
+            }
+
+            @Override
+            public void onCallFailure(Throwable throwable) {
+                callback.onError(INTERNAL_ERROR_CODE);
+            }
+        });
+    }
+
+    /**
+     * Time interval, expressed in milliseconds, in which the repository's data is considered
+     * up-to-date by the {@link #TIME_RESOLVE} policy. If said interval passed between the last
+     * information update from the network, the cache is invalidated and the request to API is
+     * executed.
+     * </p>
+     * Ie: If this method returns 2000, then 2 seconds have to elapse before the data is considered
+     * 'dirty' by the {@link #TIME_RESOLVE} policy.
+     *
+     * @return repository delta time provided for {@link #TIME_RESOLVE} queries.
+     */
+    @IntRange(from = 0)
+    protected abstract long getRefreshDelta();
+
+}
