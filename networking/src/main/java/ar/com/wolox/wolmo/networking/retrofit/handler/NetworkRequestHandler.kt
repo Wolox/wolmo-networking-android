42176c7e527d84f1a15252c0ba304fa514f87099
Lucas Miotti <lucas.miotti@wolox.com.ar>
2020-10-02 11:15:35 -0300

    Documentation added

--- networking/src/main/java/ar/com/wolox/wolmo/networking/retrofit/handler/NetworkRequestHandler.kt
+++ networking/src/main/java/ar/com/wolox/wolmo/networking/retrofit/handler/NetworkRequestHandler.kt
@@ -2,8 +2,20 @@ package ar.com.wolox.wolmo.networking.retrofit.handler
 
 import retrofit2.Response
 
+/**
+ * An adapter that converts Retrofit's responses to other, more specific, ones
+ * depending on network status codes or failures when using Kotlin's Coroutines.
+ *
+ * @param <T> the type of object expected to be returned from the API call
+ */
 object NetworkRequestHandler {
 
+    /**
+     * Static method that allows to execute requests and returns a NetworkResponse object {@link NetworkResponse}
+     * depending on HTTP response
+     *
+     * @param block is a suspend function of Response<T> type
+     */
     suspend fun <T : Response<*>> safeApiCall(block: suspend () -> T): NetworkResponse<T> =
             try {
                 val response = block.invoke()
@@ -18,7 +30,31 @@ object NetworkRequestHandler {
 }
 
 sealed class NetworkResponse<T> {
-    data class Success<T>(val body: T) : NetworkResponse<T>()
-    data class Error<T>(val body: T) : NetworkResponse<T>()
+
+    /**
+     * Successful HTTP response from the server.
+     * The server received the request, answered it and the response is not of an error type.
+     *
+     * @param response the API JSON response converted to a Java/Kotlin object.
+     * The API response code is included in the response object.
+     */
+    data class Success<T>(val response: T) : NetworkResponse<T>()
+
+    /**
+     * Successful HTTP response from the server, but has an error body.
+     * The server received the request, answered it and reported an error.
+     *
+     * @param response the API JSON response converted to a Java/Kotlin object.
+     * The API response code is included in the response object.
+     */
+    data class Error<T>(val response: T) : NetworkResponse<T>()
+
+    /**
+     * The HTTP request to the server failed on the local device, no data was transmitted.
+     * Invoked when a network or unexpected exception occurred during the HTTP request, meaning
+     * that the request couldn't be executed.
+     *
+     * @param t A Throwable with the cause of the call failure
+     */
     data class Failure<T>(val t: Throwable) : NetworkResponse<T>()
 }
